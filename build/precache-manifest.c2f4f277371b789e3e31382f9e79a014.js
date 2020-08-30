8613862cf315bf2966714e377174eb518b74095d
Matthew Forgette <mj0723@gmail.com>
2020-08-30 14:51:38 -0400

    commiting the fixed version of userhub for submission

---
+++ build/precache-manifest.c2f4f277371b789e3e31382f9e79a014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd94c2ee05549e6eef9abdfccd792d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c53dc15a6c7b2c1484d",
+    "url": "/static/css/main.1253bac1.chunk.css"
+  },
+  {
+    "revision": "fc4690d1459ce63cfd8d",
+    "url": "/static/js/2.cb8dd4da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cb8dd4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c53dc15a6c7b2c1484d",
+    "url": "/static/js/main.9e89f480.chunk.js"
+  },
+  {
+    "revision": "3712dd073b7706debb64",
+    "url": "/static/js/runtime-main.1e2863af.js"
+  }
+]);
